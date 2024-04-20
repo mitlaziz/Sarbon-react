a389e8c05aba71ebe7752c61164464d9416cb220
Max <lazizyuldoshev93@gmail.com>
2024-04-20 19:04:59 +0900

    fix: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import HomePage  from './screens/homePage';
-import ProductsPage  from './screens/productsPage';
-import OrdersPage  from './screens/ordersPage';
-import UserPage  from './screens/userPage';
-import HomeNavbar  from './components/headers/HomeNavbar';
+import HomePage from './screens/homePage';
+import ProductsPage from './screens/productsPage';
+import OrdersPage from './screens/ordersPage';
+import UserPage from './screens/userPage';
+import HomeNavbar from './components/headers/HomeNavbar';
 import OtherNavbar from './components/headers/OtherNavbar';
-import Footer  from './components/footer';
-import HelpPage  from './screens/helpPage';
+import Footer from './components/footer';
+import HelpPage from './screens/helpPage';
 import '../css/app.css';
 import '../css/navbar.css';
 import '../css/footer.css';
@@ -19,7 +19,7 @@ import Test from './screens/Test';
 function App() {
   const location = useLocation();
   console.log("location: ", location);
-  
+
   return (
     <>
       {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
@@ -37,12 +37,11 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <Test />
-          {/* <HomePage /> */}
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
-  </>
+    </>
   )
 }
 
